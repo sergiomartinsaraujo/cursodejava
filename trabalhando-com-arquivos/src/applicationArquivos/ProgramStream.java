acd4ec737023b2054c7b53b8f6d56041e7a423ba
Sergio Martins <sergio.m.araujo@hotmail.com>
2022-05-23 09:01:35 -0300

    Classes de leitura
    classes FileReader e BufferedReader

---
+++ trabalhando-com-arquivos/src/applicationArquivos/ProgramStream.java
@@ -0,0 +1,44 @@
+package applicationArquivos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ProgramStream {
+
+	public static void main(String[] args) {
+		String path = "C:\\projetos eclipse\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+
+			String line = br.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+
+			}
+		}
+		catch (IOException e) {
+			System.out.println("error: " + e.getMessage());
+		}
+		finally {
+			try {
+				if (br != null) {
+					br.close();					
+				}
+				if (fr != null) {
+					br.close();
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
