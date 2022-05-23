490fc88cb2f41a0fd4daaf11431806ebebeb4b65
Sergio Martins <sergio.m.araujo@hotmail.com>
2022-05-23 18:49:52 -0300

    Update ManipulandoPastas.java
    informações do caminho até o arquivo

--- trabalhando-com-arquivos/src/applicationArquivos/ManipulandoPastas.java
+++ trabalhando-com-arquivos/src/applicationArquivos/ManipulandoPastas.java
@@ -27,6 +27,15 @@ public class ManipulandoPastas {
 		
 		boolean success = new File (strPath + "\\subdir").mkdir();
 		System.out.println("Directory created successfuly: " + success);
+		
+		//mostrando o nome da pasta
+		System.out.println("Enter a folder path:");
+		
+		File pathFile = new File( sc.nextLine());
+		System.out.println("getName: "+ pathFile.getName());
+		System.out.println("getParent: "+ pathFile.getParent());
+		System.out.println("getPath: "+ pathFile.getPath());
+		
 		sc.close();
 
 	}
